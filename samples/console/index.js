282dd9a2ac2453a85bcf65e4f8a60e6166546b1d
configcat <developer@configcat.com>
2018-11-04 22:29:54 +0100

    update sample to v2 package [skip ci]

--- samples/console/index.js
+++ samples/console/index.js
@@ -3,7 +3,11 @@ var configcat = require("configcat-client");
 // Insert your API key
 var client = configcat.createClient("PKDVCLf-Hq-h-kCzMp-L7Q/PaDVCFk9EpmD6sLpGLltTA");
 
+var myUser = {
+    identifier : "435170f4-8a8b-4b67-a723-505ac7cdea92"
+}; 
+
 // Get your config value:
-client.getValue("keySampleText", "N/A", (value) => {
+client.getValue("keySampleText", "N/A", myUser, (value) => {
     console.log("keySampleText: " + value);
 });
