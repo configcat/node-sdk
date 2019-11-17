04222e33479df50ce5ed2cfe0e2aa6f47f1065ff
胖叔叔 <ziofat@users.noreply.github.com>
2019-11-17 21:03:48 +0800

    add proxy support with got and node-tunnel (#8)

--- src/config-fetcher.ts
+++ src/config-fetcher.ts
@@ -1,4 +1,5 @@
-import ky from 'ky-universal';
+import * as got from 'got';
+import * as tunnel from 'tunnel';
 import { IConfigFetcher } from "configcat-common";
 import { ProjectConfig } from "configcat-common/lib/ConfigServiceBase";
 import { OptionsBase } from "configcat-common/lib/ConfigCatClientOptions";
@@ -7,24 +8,39 @@ export class HttpConfigFetcher implements IConfigFetcher {
 
     fetchLogic(options: OptionsBase, lastProjectConfig: ProjectConfig, callback: (newProjectConfig: ProjectConfig) => void): void {
 
-        ky.get(options.getUrl(), {
+        let agent;
+        if (options.proxy) {
+            try {
+                const proxy = new URL(options.proxy);
+                let agentFactory = tunnel.httpsOverHttp;
+                if (proxy.protocol === 'https:') {
+                    agentFactory = tunnel.httpsOverHttps;
+                }
+                agent = agentFactory({
+                    proxy: {
+                        host: proxy.hostname,
+                        port: proxy.port,
+                    }
+                });
+            } catch {
+                options.logger.log("Failed to parse options.proxy: " + options.proxy);
+            }
+        }
+
+        got.get(options.getUrl(), {
+            agent,
             headers: {
                 "User-Agent": "ConfigCat-JS/" + options.clientVersion,
                 "X-ConfigCat-UserAgent": "ConfigCat-JS/" + options.clientVersion,
                 "If-None-Match": lastProjectConfig ? lastProjectConfig.HttpETag : null
             }
         }).then((response) => {
-            if (response && response.status === 304) {
-                callback(new ProjectConfig(new Date().getTime(), JSON.stringify(lastProjectConfig.ConfigJSON), response.headers.get('etag')));
-            } else if (response && response.status === 200) {
-                response.json().then((body) => {
-                    callback(new ProjectConfig(new Date().getTime(), JSON.stringify(body), response.headers.get('etag')));
-                }).catch((reason) => {
-                    options.logger.log("Error while parsing response JSON. Reason: " + reason);
-                    callback(lastProjectConfig);
-                });
+            if (response && response.statusCode === 304) {
+                callback(new ProjectConfig(new Date().getTime(), JSON.stringify(lastProjectConfig.ConfigJSON), response.headers.etag as string));
+            } else if (response && response.statusCode === 200) {
+                callback(new ProjectConfig(new Date().getTime(), response.body, response.headers.etag as string));
             } else {
-                options.logger.log("Failed to download config from ConfigCat. Status:" + (response && response.status) + " - " + response.statusText);
+                options.logger.log("Failed to download config from ConfigCat. Status:" + (response && response.statusCode) + " - " + response.statusMessage);
                 callback(lastProjectConfig);
             }
         }).catch((reason) => {
@@ -32,7 +48,7 @@ export class HttpConfigFetcher implements IConfigFetcher {
             if (response && response.status === 304) {
                 callback(new ProjectConfig(new Date().getTime(), JSON.stringify(lastProjectConfig.ConfigJSON), response.headers.get('etag')));
             } else {
-                options.logger.log("Failed to download config from ConfigCat. Status:" + (response && response.status) + " - " + response.statusText);
+                options.logger.log("Failed to download config from ConfigCat. Status:" + (response && response.statusCode) + " - " + response.statusMessage);
                 callback(lastProjectConfig);
             }
         });
