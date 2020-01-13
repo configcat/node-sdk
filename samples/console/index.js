31ba38cf05792adc2c8d2540a15174204aa9dc45
sige <sige@configcat.com>
2020-01-13 15:56:20 +0100

    updting logging and sample apps

--- samples/console/index.js
+++ samples/console/index.js
@@ -1,13 +1,15 @@
 var configcat = require("configcat-node");
 
-var configCatClient = configcat.createClientWithAutoPoll('PKDVCLf-Hq-h-kCzMp-L7Q/HhOWfwVtZ0mb30i9wi17GQ', { pollIntervalSeconds: 2 });
+const logger = configcat.createConsoleLogger(3); // Setting log level to 3 (= Info) to show detailed feature flag evaluation
+
+const configCatClient = configcat.createClientWithAutoPoll('PKDVCLf-Hq-h-kCzMp-L7Q/HhOWfwVtZ0mb30i9wi17GQ', { pollIntervalSeconds: 2, logger: logger });
 // You can instantiate the client with different polling modes. See the Docs: https://docs.configcat.com/docs/sdk-reference/js/#polling-modes
 
 configCatClient.getValueAsync("isAwesomeFeatureEnabled", false).then(value => {
     console.log("isAwesomeFeatureEnabled: " + value);
 });
 
-var userObject = { identifier: "#SOME-USER-ID#", email: "configcat@example.com" };
+const userObject = { identifier: "#SOME-USER-ID#", email: "configcat@example.com" };
 // Read more about the User Object: https://docs.configcat.com/docs/sdk-reference/js/#user-object
 configCatClient.getValueAsync("isPOCFeatureEnabled", false, userObject).then(value => {
     console.log("isPOCFeatureEnabled: " + value);
--- samples/expresswithdocker/index.js
+++ samples/expresswithdocker/index.js
@@ -2,32 +2,31 @@ const APIKEY = "PKDVCLf-Hq-h-kCzMp-L7Q/HhOWfwVtZ0mb30i9wi17GQ";
 const PORT = 8088;
 const SAMPLE_KEY = "isAwesomeFeatureEnabled";
 
-var express=require("express");
+var express = require("express");
 var configcat = require("configcat-node");
-var app=express();
+var app = express();
 
-let configCatClient = configcat.createClient(APIKEY);
-    
-app.get("/",function(req,res)
-{
+var logger = configcat.createConsoleLogger(3); // Setting log level to 3 (= Info) to show detailed feature flag evaluation
+
+let configCatClient = configcat.createClient(APIKEY, { logger: logger });
+
+app.get("/", function (req, res) {
     res.send("Express is running...");
 });
 
-app.get("/" + SAMPLE_KEY,async function(req,res)
-{
-    const feature2 = await configCatClient.getValueAsync(SAMPLE_KEY,  false)
+app.get("/" + SAMPLE_KEY, async function (req, res) {
+    const feature2 = await configCatClient.getValueAsync(SAMPLE_KEY, false)
     console.log(SAMPLE_KEY + ": " + feature2);
-    
-    res.send(SAMPLE_KEY+ " -> " + feature2);
+
+    res.send(SAMPLE_KEY + " -> " + feature2);
 });
 
-app.get("/keys",async function(req,res)
-{
+app.get("/keys", async function (req, res) {
     const keys = await configCatClient.getAllKeysAsync();
     console.log("keys: " + keys);
-    
+
     res.send("keys: '" + keys + "'");
 
 });
 
-var server=app.listen(PORT,function() {});
+app.listen(PORT, function () { });
