c5b0f99a16efc7b588ad11ada5323d9098880ee8
configcat <developer@configcat.com>
2020-02-01 01:11:24 +0100

    [skipci] add 'realtimeupdate' sample

---
+++ samples/realtimeupdate/index.js
@@ -0,0 +1,62 @@
+const configcatApiKey = "rlPVCCuDMlXYrZsu5t36FQ/9V3VCA4g2GepqkiG_MPZ5w";
+const pubnubSubscriberKey = "demo";
+const pubnubPublishKey = "demo";
+const configcatSettingKey = "demoswitch";
+
+// On ConfigCat Dashboard create a webhook with a following URL (HTTP-GET) 
+// https://ps.pndsn.com/publish/demo/demo/0/configcat-channel/myCallback/%7B%22CMD%22%3A%22FORCEUPDATE%22%7D
+
+
+var configcat = require("configcat-node");
+var PubNub = require('pubnub');
+
+var configCatOptions = {
+    configChanged: () => {console.log("[ConfigCat] : config changed!")}   
+}
+
+// On ConfigCat Dashboard create a webhook with a following URL (HTTP-GET) 
+// https://ps.pndsn.com/publish/demo/demo/0/configcat-channel/myCallback/%7B%22CMD%22%3A%22FORCEUPDATE%22%7D
+
+
+// ConfigCat instance
+
+var configCatClient = configcat.createClientWithManualPoll(configcatApiKey);
+
+// PubNub instance
+
+var pubnub = new PubNub({
+        subscribeKey: pubnubSubscriberKey,
+        publishKey: pubnubPublishKey,
+        uuid: "configCat-pubNub-sample"
+})
+
+pubnub.addListener({
+    message: function(m) {
+        var msg = m.message; 
+        console.log(msg);
+        if (msg.CMD == "FORCEUPDATE"){
+            configCatClient.forceRefreshAsync().then(value => {
+                console.log("[PubNub] : refresh my config cache");
+            })
+        }
+    }
+});
+
+pubnub.subscribe({
+    channels: ['configcat-channel'] 
+});
+
+function readValueFromCache(i){
+    
+    configCatClient.getValueAsync(configcatSettingKey, false).then(value => {
+        console.log(configcatSettingKey + ": " + value);
+        setTimeout(w => readValueFromCache(w), i, i);
+    });    
+}
+
+// get a latest config (only once)
+configCatClient.forceRefreshAsync().then(value => {
+
+    // start the loop to read config from cache in every 5000 ms
+    readValueFromCache(5000);
+});
