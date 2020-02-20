a33555ab4c27e43b718d0d5ab16dadf93b9d2e0b
sige <sige@configcat.com>
2020-02-20 13:04:52 +0100

    updating sample apps

--- update-samples.sh
+++ update-samples.sh
@@ -1,7 +1,10 @@
 #Update sample apps to the latest
+set -e
 cd samples/console
 npm i configcat-node@latest
 cd ../expresswithdocker
+npm i configcat-node@latest
+cd ../realtimeupdate
 npm i configcat-node@latest
 cd ../..
 git add .
