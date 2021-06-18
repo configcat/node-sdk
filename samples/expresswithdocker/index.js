f128029038ae1fba1274d099d8f55a97f3ec7724
andrew-cat <endre.toth@configcat.com>
2021-06-18 23:45:27 +0200

    tiny fix in sample

--- samples/expresswithdocker/index.js
+++ samples/expresswithdocker/index.js
@@ -5,6 +5,7 @@ const SAMPLE_KEY = "isAwesomeFeatureEnabled";
 var express = require("express");
 var configcat = require("configcat-node");
 var app = express();
+app.disable("x-powered-by");
 
 var logger = configcat.createConsoleLogger(3); // Setting log level to 3 (= Info) to show detailed feature flag evaluation
 
@@ -29,4 +30,4 @@ app.get("/keys", async function (req, res) {
 
 });
 
-app.listen(PORT, function () { });
+app.listen(PORT);
