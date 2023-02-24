8a95573b9a8512a606b16e1966f3d7b62fb97c9a
Adam Simon <adamosimoni@gmail.com>
2023-02-24 17:35:10 +0100

    Enforce consistent type exports/imports

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,6 @@
 import * as configcatcommon from "configcat-common";
-import { FlagOverrides, IAutoPollOptions, IConfigCatClient, IConfigCatLogger, ILazyLoadingOptions, IManualPollOptions, InMemoryCache, LogLevel, MapOverrideDataSource, PollingMode } from "configcat-common";
+import type { IAutoPollOptions, IConfigCatClient, IConfigCatLogger, ILazyLoadingOptions, IManualPollOptions, LogLevel } from "configcat-common";
+import { FlagOverrides, InMemoryCache, MapOverrideDataSource, PollingMode } from "configcat-common";
 import { HttpConfigFetcher } from "./config-fetcher";
 import CONFIGCAT_SDK_VERSION from "./version";
 
