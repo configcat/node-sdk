304752151f9c4abe3b2b02403fd3a5b5d8d2fea0
Adam Simon <adamosimoni@gmail.com>
2023-06-13 17:47:49 +0200

    Update samples

--- samples/customcache/configcat-redis-cache.js
+++ samples/customcache/configcat-redis-cache.js
@@ -15,38 +15,23 @@ function configcatRedisCache(redisClientOps) {
     console.log("Redis error: " + err);
     this.isRedisAvailable = false;
   });
-
-  this.lastCacheItems = {};
 }
 
 configcatRedisCache.prototype.get = async function(key) {
 
-  if (!this.isRedisAvailable) {
-    return this.lastCacheItems[key];
-  }
-
-  try {
+  if (this.isRedisAvailable) {
     const getAsync = util.promisify(this.cacheClient.get).bind(this.cacheClient);
 
-    return JSON.parse(await getAsync(key));
-  }
-  catch (e) {
-    console.error("Cache read failed!\n" + e);
-    return this.lastCacheItems[key];
+    return await getAsync(key);
   }
 };
 
 configcatRedisCache.prototype.set = async function(key, item) {
 
-  this.lastCacheItems[key] = item;
-
-  try {
+  if (this.isRedisAvailable) {
     const setAsync = util.promisify(this.cacheClient.set).bind(this.cacheClient);
 
-    await setAsync(key, JSON.stringify(item));
-  }
-  catch (e) {
-    console.error("Cache write failed!\n" + e);
+    await setAsync(key, item);
   }
 };
 
