918c2831dea276400862ed764c2a60eeea4da227
adams85 <31276480+adams85@users.noreply.github.com>
2023-11-24 16:11:47 +0100

    Prepare v11.0.0 (Catnip) release (#68)
    * Update configcat-common v9.0.0
    
    * Fix dependency vulnerabilities
    
    * Bump version
    
    * Update samples
    
    * Run tests on Node.js 20

--- test/client-tests.ts
+++ test/client-tests.ts
@@ -8,7 +8,7 @@ describe("ConfigCatClient tests", () => {
   for (const pollingMode of [PollingMode.AutoPoll, PollingMode.LazyLoad, PollingMode.ManualPoll]) {
     it(`getClient() should createInstance with ${PollingMode[pollingMode]}`, () => {
 
-      const client: IConfigCatClient = configcatClient.getClient("SDKKEY", pollingMode);
+      const client: IConfigCatClient = configcatClient.getClient("SDKKEY-890123456789012/1234567890123456789012", pollingMode);
 
       assert.isDefined(client);
 
